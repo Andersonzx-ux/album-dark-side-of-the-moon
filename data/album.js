5c713e5b45295c180900b8b8fefddbfbde1f20ad
Anderson <goesanderson074@gmail.com>
2026-09-02 14:22:16 -0300

    Add files via upload

---
+++ data/album.js
@@ -0,0 +1,292 @@
+window.album = {
+    title: "The Dark Side of the Moon",
+    artist: "Pink Floyd",
+    releaseDate: "1 de março de 1973",
+    recordingDate: "31 de maio de 1972 – 9 de fevereiro de 1973",
+    genre: "Art Rock, Space Rock, Rock Progressivo e Rock Psicodélico",
+    labels: ["Harvest, Capitol, CBS/Columbia"],
+    cover: "assets/cover.jpg",
+    description: "",
+    totalDuration: "42:50",
+    awards: [
+        { award: "Grammy Awards — Best Engineered Recording, Non-Classical", result: "Indicado", year: "1974", category: "Melhor Engenharia de Som em Álbum (Não-Clássica)", description: "Reconhecimento pela consistência do álbum." },
+        { award: "National Recording Registry (EUA)", result: "Selecionado para preservação", year: "2012", category: "Álbuns selecionados para preservação", description: "Gravações consideradas culturalmente, historicamente ou esteticamente significativas para os Estados Unidos." },
+        { award: "RIAA — Platinum", result: "Certificado", year: "1990", category: "Vendas", description: "11× Platina" },
+        { award: "RIAA — Platinum", result: "Certificado", year: "1998", category: "Vendas", description: "15× Platina" },
+        { award: "Grammy Hall of Fame", result: "Incluído", year: "1999", category: "Academia de Gravação", description: "Gravações historicamente importantes e de impacto duradouro." },
+    ],
+    technicalCredits: {
+        production: ["Pink Floyd"],
+        direction: ["Alan Parsons"],
+        arrangements: ["Roger Waters, David Gilmour, Richard Wright e Nick Mason"],
+        recording: ["EMI Studios (Londres)"],
+        mixing: ["Chris Thomas e Alan Parsons"],
+        mastering: ["Walter Lima"],
+        studios: ["EMI Studios (Londres)"],
+        photography: "Hipgnosis",
+        artwork: "Storm Thorgerson / Hipgnosis"
+    },
+    tracks: [
+        {
+            number: 1,
+            title: "Speak to Me",
+            duration: "1:07",
+            composers: ["Nick Mason"],
+            audio: "audio/01.flac",
+            lyrics: `[00:31.28] I've always been mad for fucking years, absolutely years
+[00:34.56] Been over the edge for yonks
+[00:36.23] Been working me buns off for bands
+[00:39.93] I've always been mad, I know I've been mad like the most of us
+[00:45.05] Very hard to explain why you're mad, even if you're not mad
+[00:49.17] `
+        },
+        {
+            number: 2,
+            title: "Breathe (In the Air)",
+            duration: "2:49",
+            composers: ["David Gilmour, Roger Waters, Richard Wright"],
+            audio: "audio/02.flac",
+            lyrics: `[01:20.55] Breathe, breathe in the air
+[01:26.56] 
+[01:28.84] Don't be afraid to care
+[01:35.78] Leave, but don't leave me
+[01:42.55] Look around choose your own ground
+[01:49.89] For long you live and high you fly
+[01:53.42] And smiles you'll give and tears you'll cry
+[01:57.38] And all you touch and all you see
+[02:01.03] Is all your life will ever be
+[02:05.88] Run, rabbit, run
+[02:12.45] Dig that hole, forget the sun
+[02:19.93] And when at last the work is done
+[02:26.98] Don't sit down it's time to dig another one
+[02:34.47] For long you live and high you fly
+[02:38.15] But only if you ride the tide
+[02:41.76] And balanced on the biggest wave
+[02:45.56] You race towards an early grave
+[02:48.79] `
+        },
+        {
+            number: 3,
+            title: "On the Run",
+            duration: "3:45",
+            composers: ["David Gilmour, Roger Waters"],
+            audio: "audio/03.flac",
+            lyrics: ``
+        },
+        {
+            number: 4,
+            title: "Time",
+            duration: "6:53",
+            composers: ["David Gilmour, Nick Mason, Roger Waters, Richard Wright"],
+            audio: "audio/04.flac",
+            lyrics: `[02:18.65] Ticking away the moments that make up a dull day
+[02:25.41] Fritter and waste the hours in an off-hand way
+[02:33.26] Kicking around on a piece of ground in your hometown
+[02:40.41] Waiting for someone or something to show you the way
+[02:47.47] Tired of lying in the sunshine, staying home to watch the rain
+[02:55.03] You are young and life is long, and there is time to kill today
+[03:03.18] And then one day you find ten years have got behind you
+[03:10.11] No one told you when to run, you missed the starting gun
+[04:44.05] And you run, and you run to catch up with the Sun, but it's sinking
+[04:51.83] Racing around to come up behind you again
+[04:58.72] The Sun is the same in a relative way but you're older
+[05:05.76] Shorter of breath and one day closer to death
+[05:13.06] Every year is getting shorter, never seem to find the time
+[05:20.46] Plans that either come to naught, or half a page of scribbled lines
+[05:28.12] Hanging on in quiet desperation is the English way
+[05:35.57] The time is gone, the song is over, thought I'd something more to say
+[05:59.93] Home, home again
+[06:07.30] I like to be here when I can
+[06:15.63] And when I come home, cold and tired
+[06:22.72] It's good to warm my bones beside the fire
+[06:30.97] Far away across the field, the tolling of the iron bell
+[06:38.79] Calls the faithful to their knees
+[06:43.10] To hear the softly spoken magic spells
+[06:48.71] `
+        },
+        {
+            number: 5,
+            title: "The Great Gig in the Sky",
+            duration: "4:44",
+            composers: ["Richard Wright, Clare Torry"],
+            audio: "audio/05.flac",
+            lyrics: `[00:38.82] I am not frightened of dying, you know
+[00:42.06] Any time will do, I don't mind
+[00:44.70] Why should I be frightened of dying?
+[00:48.28] 
+[00:50.37] There's no reason for it, you've gotta go sometime
+[00:54.17] 
+[02:34.74] I never said I was frightened of dying
+[02:35.85] `
+        },
+        {
+            number: 6,
+            title: "Money",
+            duration: "6:23",
+            composers: ["Roger Waters"],
+            audio: "audio/06.flac",
+            lyrics: `[00:40.34] Money, get away
+[00:46.74] Get a good job with more pay and you're okay
+[00:54.09] Money, it's a gas
+[01:00.59] Grab that cash in both hands and make a stash
+[01:07.85] New car, caviar, four-star Daydream
+[01:11.74] Think I'll buy me a football team
+[01:17.64] 
+[01:21.31] Money, get back
+[01:27.63] I'm all right Jack, but keep your hands off of my stack
+[01:34.86] Money, it's a hit
+[01:41.05] Don't give me that do-goody-good bullshit
+[01:46.95] I'm in the High-Fidelity First Class Traveling Set
+[01:52.50] And I think I need a LearJet
+[01:56.39] 
+[05:11.06] Money, it's a crime
+[05:17.00] Share it fairly but don't take a slice of my pie
+[05:24.09] Money, so they say
+[05:30.16] Is the root of all evil today
+[05:37.18] But if you ask for a rise
+[05:39.57] It's no surprise that they giving none away
+[05:45.12] Away, away-ay, away
+[05:50.72] Away, away, away away
+[06:19.76] `
+        },
+        {
+            number: 7,
+            title: "Us and Them",
+            duration: "7:49",
+            composers: ["Richard Wright, Roger Waters"],
+            audio: "audio/07.flac",
+            lyrics: `[01:38.12]Us and them
+[01:43.36]
+[01:52.85]And after all, we're only ordinary men
+[01:57.06]
+[02:11.83]Me and you
+[02:16.74]
+[02:26.63]God only knows, it's not what we would choose to do
+[02:32.75]
+[02:45.54]Forward he cried from the rear
+[02:50.00]And the front rank died
+[02:51.31]
+[02:58.30]The General sat, and the lines on the map
+[03:04.41]Moved from side to side
+[03:11.32]Black and blue
+[03:16.44]
+[03:26.13]And who knows which is which? And who is who?
+[03:29.11]
+[03:44.89]Up and down
+[03:49.76]
+[03:59.58]And in the end, it's only round and round and round
+[04:06.84]
+[04:17.97]Haven't you heard it's a battle of words?
+[04:23.55]The poster bearer cried
+[04:30.61]"Listen son", said the man with the gun
+[04:36.13]"There's room for you inside"
+[04:38.62]
+[05:00.64](They're maybe gonna kill ya)
+[05:02.06](So like, if you give 'em a quick, short, sharp, shock, they don't do it again))
+[05:07.69](Dig it? I mean he get off light, come to think)
+[05:10.28](Of rationalizing it, once)
+[05:11.30](It's only a difference, but why go and ruin it?)
+[05:14.10](I mean good manners don't cost nothing, do they, eh?)
+[05:14.41]
+[06:14.46]Down and out
+[06:18.93]
+[06:29.14]It can't be helped but there's a lot of it about
+[06:33.16]
+[06:47.79]With, without
+[06:54.32]
+[07:02.19]And who'll deny it's what the fighting's all about
+[07:06.31]
+[07:20.43]Out of the way, it's a busy day
+[07:25.90]I've got things on my mind
+[07:32.66]For want of the price of tea and a slice
+[07:38.59]The old man died
+[07:47.27]`
+        },
+        {
+            number: 8,
+            title: "Any Colour You Like",
+            duration: "3:26",
+            composers: ["David Gilmour, Nick Mason, Richard Wright"],
+            audio: "audio/08.flac",
+            lyrics: ``
+        },
+        {
+            number: 9,
+            title: "Brain Damage",
+            duration: "3:46",
+            composers: ["Roger Waters"],
+            audio: "audio/09.flac",
+            lyrics: `[00:15.35] The lunatic is on the grass
+[00:20.53] 
+[00:22.55] The lunatic is on the grass
+[00:27.59] 
+[00:29.90] Remembering games
+[00:33.26] And daisy chains and laughs
+[00:36.61] Got to keep the loonies on the path
+[00:41.45] 
+[00:44.29] The lunatic is in the hall
+[00:51.56] The lunatics are in my hall
+[00:56.73] 
+[00:58.98] The paper holds their folded faces to the floor
+[01:05.67] And every day the paper boy brings more
+[01:11.16] 
+[01:17.53] And if the dam breaks open many years too soon
+[01:24.32] And if there is no room upon the hill
+[01:29.01] 
+[01:31.57] And if your head explodes with dark forebodings too
+[01:38.06] I'll see you on the dark side of the moon
+[01:43.42] 
+[01:48.64] The lunatic is in my head
+[01:56.46] The lunatic is in my head
+[02:03.71] You raise the blade
+[02:07.26] You make the change
+[02:10.98] You rearrange me 'till I'm sane
+[02:16.04] 
+[02:18.36] You lock the door
+[02:21.05] And throw away the key
+[02:25.14] And there's someone in my head, but it's not me
+[02:30.16] 
+[02:36.04] And if the cloud bursts thunder in your ear
+[02:42.90] You shout and no one seems to hear
+[02:47.99] 
+[02:50.18] And if the band you're in starts playing different tunes
+[02:56.94] I'll see you on the dark side of the moon
+[03:02.51] 
+[03:21.99] "I can't think of anything to say except... HaHaHa!"
+[03:27.03] `
+        },
+        {
+            number: 10,
+            title: "Eclipse",
+            duration: "2:12",
+            composers: ["Roger Waters"],
+            audio: "audio/10.flac",
+            lyrics: `[00:10.66] All that you touch
+[00:13.31] All that you see
+[00:16.15] All that you taste
+[00:19.15] All you feel
+[00:21.33] All that you love
+[00:23.88] All that you hate
+[00:26.64] All you distrust
+[00:29.69] All you save
+[00:31.82] All that you give
+[00:34.24] All that you deal
+[00:37.08] All that you buy
+[00:39.15] Beg, borrow or steal
+[00:42.03] All you create
+[00:44.73] All you destroy
+[00:47.48] All that you do
+[00:49.75] And all that you say
+[00:52.85] And all that you eat
+[00:55.27] And everyone you meet (everyone you meet)
+[00:58.22] All that you slight
+[01:00.41] And everyone you fight
+[01:03.01] All that is now
+[01:05.75] All that is gone
+[01:08.07] All that's to come
+[01:10.92] And everything under the sun is in tune
+[01:15.92] But the sun is eclipsed by the moon
+[01:19.13] `
+        }
+    ]
+};
